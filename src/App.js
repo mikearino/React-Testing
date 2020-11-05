a1136c7178988495b238d5f21a825b7d1be8b70f
Mike Lofgren <mikeylofgren@gmail.com>
2020-11-04 21:41:42 -0800

    add int test.

--- src/App.js
+++ src/App.js
@@ -3,3 +3,7 @@ export const add = (x, y) => {
 }
 
 // export const add = (x, y) => x + y
+
+export const total = (shipping, subTotal) => {
+return "$" + add(shipping, subTotal)
+}
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
-import { add } from './App'
+import { add, total } from './App'
 
 // Unit test
 // Only tests one thing
+// const value = add(1,2)
 test('add',() => {
-  // const value = add(1,2)
   expect(add(1,2)).toBe(3);
   expect(add(5,2)).toBe(7);
+})
+
+test('total', () => {
+  expect(total(5,20)).toBe('$25')
 })
