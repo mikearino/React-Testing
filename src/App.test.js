83cb35e7d52a8c676f417ce00b0a52b8ed866a68
Mike Lofgren <mikeylofgren@gmail.com>
2020-11-05 21:21:19 -0800

    add mock functions.

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,18 @@
-import { add, total } from './App'
+import { total } from './App'
+
+const add = jest.fn((x,y) => 3)
 
 // Unit test
 // Only tests one thing
-// const value = add(1,2)
+
 test('add',() => {
   expect(add(1,2)).toBe(3);
-  expect(add(5,2)).toBe(7);
+  expect(add).toHaveBeenCalledWith(1,2)
+  // expect(add(5,2)).toBe(7);
 })
 
-test('total', () => {
-  expect(total(5,20)).toBe('$25')
-})
+// Integration tests
+// Tests things working together
+// test('total', () => {
+//   expect(total(5,20)).toBe('$25')
+// })
